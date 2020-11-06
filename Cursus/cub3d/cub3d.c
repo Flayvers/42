65d3d09e958ca69609fb6a04b4b588e3b9e98398
Shadow <nahel.rhrara@gmail.com>
2020-11-06 19:57:43 +0100

    06/11

--- Cursus/cub3d/cub3d.c
+++ Cursus/cub3d/cub3d.c
@@ -18,13 +18,13 @@ int				main(int ac, char **av)
     t_window 	*win;
 
     printf("0");
-    //mlx = mlx_init();
+    mlx = mlx_init();
     printf("1");
-    //win->set_name = av[1];
+    win->set_name = av[1];
     printf("2");
     //parser(win);
     printf("3");
-    //win->mlx_win = mlx_new_window(mlx, win->heigh, win->width, "Cub3D");
-    //mlx_loop(mlx);
+    win->mlx_win = mlx_new_window(mlx, win->heigh, win->width, "Cub3D");
+    mlx_loop(mlx);
     return(0);
 } 
